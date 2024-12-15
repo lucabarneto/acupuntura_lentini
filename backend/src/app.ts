ad39cee83c0a2085472fe871b25a0c3395e7202f
Luca Barneto <luca.barneto.uiux@gmail.com>
2024-12-15 17:16:57 -0300

    Initializing Express server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,12 @@
-console.log("Hola Mundo!");
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log("Hola Mundo!");
+});
