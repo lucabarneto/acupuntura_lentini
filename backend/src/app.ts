cb7211409706984ca5e3f3dbd12ed69ac4884b8c
Luca Barneto <luca.barneto.uiux@gmail.com>
2025-01-02 21:29:15 -0300

    adding index.routes to my project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,6 @@
 import express from "express";
 import DatabaseConnection from "./config/database_connection.config.ts";
-import patientRouter from "./routes/patients.routes.ts";
-import chiefComplaintRouter from "./routes/chiefComplaints.routes.ts";
-import sessionRouter from "./routes/sessions.routes.ts";
-import appointmentRouter from "./routes/appointments.routes.ts";
+import { router } from "./routes/index.routes.ts";
 import { logger } from "./utils/logger.ts";
 import { errorHandler } from "./middlewares/errorHandler.ts";
 
@@ -15,10 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /* Routes middlewares */
-app.use("/api/patients", patientRouter);
-app.use("/api/chiefcomplaints", chiefComplaintRouter);
-app.use("/api/sessions", sessionRouter);
-app.use("/api/appointments", appointmentRouter);
+app.use("/api/patients", router.patientRouter);
+app.use("/api/chiefcomplaints", router.chiefComplaintRouter);
+app.use("/api/sessions", router.sessionRouter);
+app.use("/api/appointments", router.appointmentRouter);
 
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
---
+++ backend/src/routes/index.routes.ts
@@ -0,0 +1,11 @@
+import appointmentRouter from "./appointments.routes.ts";
+import chiefComplaintRouter from "./chiefComplaints.routes.ts";
+import patientRouter from "./patients.routes.ts";
+import sessionRouter from "./sessions.routes.ts";
+
+export const router = {
+  appointmentRouter,
+  chiefComplaintRouter,
+  patientRouter,
+  sessionRouter,
+};
