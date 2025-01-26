1e7de2b577a53454bf5eb6742a641bfad8771338
Luca Barneto <luca.barneto.uiux@gmail.com>
2025-01-26 12:48:23 -0300

    Adding authentication middleware for future use

---
+++ backend/src/middlewares/authentication.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import passport from "passport";
+
+export const authenticate = (
+  strategy: "login" | "jwt",
+  options: passport.AuthenticateOptions
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate(
+      strategy,
+      options,
+      function (error: any, user: Express.User | false | null) {
+        try {
+          if (error) throw error;
+
+          if (user!) throw new Error("Unauthenticated");
+
+          req.user = user!;
+
+          next();
+        } catch (err) {
+          next(err);
+        }
+      }
+    );
+  };
+};
